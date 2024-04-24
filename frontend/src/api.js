ef10f7bb568687f04e14ed4918e04835d8f30a57
Lg026 <130603739+Lg026@users.noreply.github.com>
2024-04-23 19:11:01 -0700

    connected frontend to backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,13 +1,12 @@
 import axios from 'axios'
 import { ACCESS_TOKEN } from './constants'
 
-// const api = axios.create({
-//     baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
-// })
+const apiUrl = "/choreo-apis/animetome/backend/rest-api-be2/v1.0"
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL 
+    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
 })
+
 
 api.interceptors.request.use(
     (config) => {
