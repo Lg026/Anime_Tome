656ffbf10dc91ef3270b5ae870195637457ef2e1
Luis <guzman.lg22@gmail.com>
2026-02-20 20:23:10 -0800

    fixed fallback

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,17 +1,13 @@
 import axios from 'axios'
 import { ACCESS_TOKEN } from './constants'
 
-const apiUrl = "http://127.0.0.1:8000";
-
-
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
+    baseURL: import.meta.env.VITE_API_URL,
 })
-
 
 api.interceptors.request.use(
     (config) => {
-        const token = localStorage.getItem(ACCESS_TOKEN);
+        const token = localStorage.getItem(ACCESS_TOKEN)
         if (token) {
             config.headers.Authorization = `Bearer ${token}`
         }
@@ -23,4 +19,5 @@ api.interceptors.request.use(
 )
 
 export default api
+
 
